5c3c36e7e8e119768d9e013acf8ff2f66dce13aa
Sgarra <sgarraclaudia0@gmai.com>
2023-03-20 19:11:27 +0100

    LocalOutlierFactor completo e ABOD, OCSVM scripts

---
+++ competitors/ABOD/ABOD.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import pandas as pd
+import sys
+import time
+import pyod
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix
+from pyod.models.abod import ABOD
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+n_clusters = [5, 10, 25, 50, 100]
+num_estimators = [10, 20, 50]
+
+columns = ["date","ClusterLatitude","ClusterLongitude","Delay","Percentage","InPanic","InCongestion",
+           "DestinationAimedArrivalTime","OriginAimedDepartureTime","HeadwayService_False","anomaly"]
+
+for n in n_clusters:
+    dataset_name = "aggr_" + str(n)
+    # data pipeline
+    filename_dates = "dates/" + dataset_name + "_100.txt"
+    dates = pd.read_csv(filename_dates, delimiter=' ')
+
+    filename_data_train = "Datasets/" + dataset_name + ".txt.red.txt"
+    filename_data_pred = "Datasets/" + dataset_name + ".txt.red_test.txt"
+
+    data_train_full = pd.read_csv(filename_data_train,
+                parse_dates=['date'],
+                na_values=[0.0])
+
+    data_train_full.fillna(0, inplace=True)
+
+    data_train_full = data_train_full.loc[:, columns]
+    #________________________________________________________
+    data_pred_full = pd.read_csv(filename_data_pred,
+                parse_dates=['date'],
+                na_values=[0.0])
+
+    data_pred_full.fillna(0, inplace=True)
+
+    ground_truth_cols = ['anomaly']
+
+    data_gt = data_pred_full.loc[:, ground_truth_cols]
+
+    data_pred_full = data_pred_full.loc[:, columns]
+    #________________________________________________________
+
+    clf = ABOD()
+
+
+    setting_name = dataset_name + "_ABOD"
+
+    if not os.path.exists('logs/' + setting_name):
+        os.makedirs('logs/' + setting_name)
+
+    metrics_ABOD_weighted = []
+    metrics_ABOD_micro = []
+    metrics_ABOD_macro = []
+
+    list_of_rows_ABOD = []
+    pred_real_ABOD = []
+
+    
+    train_data = data_train_full.drop(['date'], axis=1)
+    print("Selected training data length: " + str(len(train_data)))
+
+    # current day data for prediction
+    data_pred = data_pred_full.drop(['date'], axis=1)
+    print(np.shape(data_pred))
+
+    # ground truth (anomaly yes/ no)
+    real_classes = data_gt.drop(['date'], axis=1)
+    real_classes = np.array(real_classes).flatten()
+
+    start = time.time()
+    ABOD_model = clf.fit(train_data)
+    print("ABOD model trained")
+    stop = time.time()
+    print(f"Training time: {stop - start}s")
+    print("Isolation Forest trained")
+
+    ABOD_model_predictions = ABOD_model.predict(data_pred)
+    ABOD_model_predictions = np.where(ABOD_model_predictions == 1, 0, ABOD_model_predictions)
+    ABOD_model_predictions = np.where(ABOD_model_predictions == -1, 1, ABOD_model_predictions)
+
+    pred_real_ABOD = np.hstack(
+        (np.reshape(ABOD_model_predictions, (np.shape(ABOD_model_predictions)[0], 1)),
+        np.reshape(real_classes, (np.shape(real_classes)[0], 1))))
+
+    confusion_matrix_ABOD = confusion_matrix(real_classes, ABOD_model_predictions, labels=[0, 1])
+    ABOD_tn, ABOD_fp, ABOD_fn, ABOD_tp = confusion_matrix_ABOD.ravel()
+
+    [precision_ABOD_weighted, recall_ABOD_weighted, fscore_ABOD_weighted, support_ABOD_weighted] = precision_recall_fscore_support(real_classes, ABOD_model_predictions, average='weighted')
+    [precision_ABOD_micro, recall_if_micro, fscore_if_micro, support_if_micro] = precision_recall_fscore_support(real_classes, ABOD_model_predictions, average='micro')
+    [precision_ABOD_macro, recall_if_macro, fscore_if_macro, support_if_macro] = precision_recall_fscore_support(real_classes, ABOD_model_predictions, average='macro')
+
+    print("Anomaly detection with ABOD model..")
+    metrics_ABOD_weighted.append(str(precision_ABOD_weighted) + "," + str(recall_ABOD_weighted) + "," + str(fscore_ABOD_weighted))
+    metrics_ABOD_micro.append(str(precision_ABOD_micro) + "," + str(recall_if_micro) + "," + str(fscore_if_micro))
+    metrics_ABOD_macro.append(str(precision_ABOD_macro) + "," + str(recall_if_macro) + "," + str(fscore_if_macro))
+
+    row_if = [ABOD_tn, ABOD_fp, ABOD_fn, ABOD_tp]
+
+    list_of_rows_ABOD.append(row_if)
+
+    # Saving predicted and real classes
+    pred_real_if = np.hstack((np.reshape(ABOD_model_predictions, (np.shape(ABOD_model_predictions)[0], 1)),
+                                np.reshape(real_classes, (np.shape(real_classes)[0], 1))))
+
+    np.savetxt('logs/' + setting_name + "/ABOD.preds"  + ".log", pred_real_if, delimiter=',', fmt='%s')
+    
+
+    if not os.path.exists('logs/' + setting_name):
+        os.makedirs('logs/' + setting_name)
+
+    # Saving confusion matrices
+    import csv
+
+    with open('logs/' + setting_name + '/ABOD.matrix.csv', "w+", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(list_of_rows_ABOD)
+
+    # Saving predicted and real classes
+    np.savetxt('logs/' + setting_name + "/ABOD.preds.log", pred_real_ABOD, delimiter=',', fmt='%s')
+
+    # Saving metrics
+    np.savetxt('logs/' + setting_name + "/ABOD.metrics.weighted.log", metrics_ABOD_weighted, delimiter=',', fmt='%s')
+    np.savetxt('logs/' + setting_name + "/ABOD.metrics.macro.log", metrics_ABOD_macro, delimiter=',', fmt='%s')
+    np.savetxt('logs/' + setting_name + "/ABOD.metrics.micro.log", metrics_ABOD_micro, delimiter=',', fmt='%s')
+
+
+    #sys.exit(1)
---
+++ competitors/OCSVM/OCSVM.py
@@ -0,0 +1,132 @@
+import os
+import numpy as np
+import pandas as pd
+import sys
+import time
+from datetime import datetime, timedelta
+from sklearn.svm import OneClassSVM
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix
+
+np.set_printoptions(threshold=sys.maxsize)
+
+n_clusters = [5, 10, 25, 50, 100]
+nu = [0.1, 0.5, 1.0]
+kernel = ["linear","rbf"]
+gamma = 'auto'
+
+columns = ["date","ClusterLatitude","ClusterLongitude","Delay","Percentage","InPanic","InCongestion",
+           "DestinationAimedArrivalTime","OriginAimedDepartureTime","HeadwayService_False","anomaly"]
+
+for n in n_clusters:
+    dataset_name = "aggr_" + str(n)
+    filename_data_train = "Datasets/" + dataset_name + ".txt.red.txt"
+    filename_data_pred = "Datasets/" + dataset_name + ".txt.red_test.txt"
+
+    data_train_full = pd.read_csv(filename_data_train,
+                parse_dates=['date'],
+                na_values=[0.0])
+
+    data_train_full.fillna(0, inplace=True)
+
+    data_train_full = data_train_full.loc[:, columns]
+    #________________________________________________________
+    data_pred_full = pd.read_csv(filename_data_pred,
+                parse_dates=['date'],
+                na_values=[0.0])
+
+    data_pred_full.fillna(0, inplace=True)
+
+    ground_truth_cols = ['anomaly']
+
+    data_gt = data_pred_full.loc[:, ground_truth_cols]
+
+    data_pred_full = data_pred_full.loc[:, columns]
+    #________________________________________________________
+
+
+    for n in nu:
+        for k in kernel:
+            one_class_svm = OneClassSVM(nu=n, kernel=k, gamma=gamma)
+
+            setting_name = dataset_name + "_OCSVM_nu_" + str(n) + "_kernel_" + str(k)
+
+            if not os.path.exists('logs/' + setting_name):
+                os.makedirs('logs/' + setting_name)
+
+            metrics_ocsvm_weighted = []
+            metrics_ocsvm_micro = []
+            metrics_ocsvm_macro = []
+
+            list_of_rows_ocsvm = []
+            pred_real_ocsvm = []
+
+
+            train_data = data_train_full.drop(['date'], axis=1)
+            print("Selected training data length: " + str(len(train_data)))
+
+            # current day data for prediction
+            data_pred = data_pred_full.drop(['date'], axis=1)
+            print(np.shape(data_pred))
+
+            # ground truth (anomaly yes/ no)
+            real_classes = data_gt.copy()
+            real_classes = np.array(real_classes).flatten()
+
+            start = time.time()
+            ocsvm_model = one_class_svm.fit(train_data)
+            stop = time.time()
+            print(f"Training time: {stop - start}s")
+            print("OCSVM trained")
+
+            ocsvm_predictions = ocsvm_model.predict(data_pred)
+            ocsvm_predictions = np.where(ocsvm_predictions == 1, 0.0, ocsvm_predictions)
+            ocsvm_predictions = np.where(ocsvm_predictions == -1, 1.0, ocsvm_predictions)
+
+            pred_real_lof = np.hstack(
+                (np.reshape(ocsvm_predictions, (np.shape(ocsvm_predictions)[0], 1)),
+                np.reshape(real_classes, (np.shape(real_classes)[0], 1))))
+
+            confusion_matrix_ocsvm = confusion_matrix(real_classes, ocsvm_predictions)
+            ocsvm_tn, ocsvm_fp, ocsvm_fn, ocsvm_tp = confusion_matrix_ocsvm.ravel()
+
+            [precision_ocsvm_weighted, recall_ocsvm_weighted, fscore_ocsvm_weighted, support_ocsvm_weighted] = precision_recall_fscore_support(real_classes, ocsvm_predictions, average='weighted')
+            [precision_ocsvm_micro, recall_ocsvm_micro, fscore_ocsvm_micro, support_ocsvm_micro] = precision_recall_fscore_support(real_classes, ocsvm_predictions, average='micro')
+            [precision_ocsvm_macro, recall_ocsvm_macro, fscore_ocsvm_macro, support_ocsvm_macro] = precision_recall_fscore_support(real_classes, ocsvm_predictions, average='macro')
+
+            print("Anomaly detection with OCSVM model..")
+            metrics_ocsvm_weighted.append(str(precision_ocsvm_weighted) + "," + str(recall_ocsvm_weighted) + "," + str(fscore_ocsvm_weighted))
+            metrics_ocsvm_micro.append(str(precision_ocsvm_micro) + "," + str(recall_ocsvm_micro) + "," + str(fscore_ocsvm_micro))
+            metrics_ocsvm_macro.append(str(precision_ocsvm_macro) + "," + str(recall_ocsvm_macro) + "," + str(fscore_ocsvm_macro))
+
+            row_ocsvm = [ocsvm_tn,ocsvm_fp, ocsvm_fn, ocsvm_tp]
+
+            list_of_rows_ocsvm.append(row_ocsvm)
+
+            # Saving predicted and real classes
+            pred_real_ocsvm = np.hstack((np.reshape(ocsvm_predictions, (np.shape(ocsvm_predictions)[0], 1)),
+                                    np.reshape(real_classes, (np.shape(real_classes)[0], 1))))
+
+            np.savetxt('logs/' + setting_name + "/ocsvm.preds_" + ".log", pred_real_ocsvm, delimiter=',', fmt='%s')
+            
+
+            if not os.path.exists('logs/' + setting_name):
+                os.makedirs('logs/' + setting_name)
+
+            # Saving confusion matrices
+            import csv
+
+            with open('logs/' + setting_name + '/ocsvm.matrix.csv', "w+", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerows(list_of_rows_ocsvm)
+
+            # Saving predicted and real classes
+            np.savetxt('logs/' + setting_name + "/lof.preds.log", pred_real_ocsvm, delimiter=',', fmt='%s')
+
+            # Saving metrics
+            np.savetxt('logs/' + setting_name + "/ocsvm.metrics.weighted.log", metrics_ocsvm_weighted, delimiter=',', fmt='%s')
+            np.savetxt('logs/' + setting_name + "/ocsvm.metrics.macro.log", metrics_ocsvm_macro, delimiter=',', fmt='%s')
+            np.savetxt('logs/' + setting_name + "/ocsvm.metrics.micro.log", metrics_ocsvm_micro, delimiter=',', fmt='%s')
+
+
+            #sys.exit(1)
