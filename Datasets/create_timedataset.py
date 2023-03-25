257067d80ed889fc32b9c3d6e3539e8142a270b4
andmon97 <andrea.montemurro97@gmail.com>
2023-03-25 12:05:35 +0100

    time dataset script creation

--- Datasets/create_timedataset.py
+++ Datasets/create_timedataset.py
@@ -6,10 +6,14 @@ import time
 
 np.set_printoptions(threshold=sys.maxsize)
 
-n_clusters = [5, 10, 25, 50, 100] 
+n_clusters = [5, 10, 25, 50, 100]
 
 columns = ["date","ClusterLatitude","ClusterLongitude","Delay","Percentage","InPanic","InCongestion",
            "DestinationAimedArrivalTime","OriginAimedDepartureTime","HeadwayService_False","anomaly"]
+
+new_columns = ["year","month","day","hour","minute",
+               "ClusterLatitude","ClusterLongitude","Delay","Percentage","InPanic","InCongestion",
+               "DestinationAimedArrivalTime","OriginAimedDepartureTime","HeadwayService_False","anomaly"]
 
 for n in n_clusters:
     dataset_name = "aggr_" + str(n)
@@ -31,7 +35,6 @@ for n in n_clusters:
     data_train_full['day'] = data_train_full['date'].dt.day
     data_train_full['hour'] = data_train_full['date'].dt.hour
     data_train_full['minute'] = data_train_full['date'].dt.minute
-    data_train_full['second'] = data_train_full['date'].dt.second
     #________________________________________________________
     data_pred_full = pd.read_csv(filename_data_pred,
                 parse_dates=['date'],
@@ -51,17 +54,21 @@ for n in n_clusters:
     data_pred_full['day'] = data_pred_full['date'].dt.day
     data_pred_full['hour'] = data_pred_full['date'].dt.hour
     data_pred_full['minute'] = data_pred_full['date'].dt.minute
-    data_pred_full['second'] = data_pred_full['date'].dt.second
     #________________________________________________________
 
     # remove the "date" column from both data_train_full and data_pred_full
     data_train_full.drop(['date'], axis=1, inplace=True)
     data_pred_full.drop(['date'], axis=1, inplace=True)
 
-    # save the two datafreames to csv files 
-    data_train_full.to_csv("Datasets/" + "ST_" + dataset_name + ".txt.red.csv", index=False)
-    data_pred_full.to_csv("Datasets/" + "ST_" + dataset_name + ".txt.red_test.csv", index=False)
+    # create new data_train_full and data_pred_full with the new columns
+    new_data_train_full = data_train_full.loc[:, new_columns]
+    new_data_pred_full = data_pred_full.loc[:, new_columns]
 
-
+    # copy the data_train_full and data_pred_full to new variables with the new columns
+    for col in new_columns:
+        new_data_train_full[col] = data_train_full[col]
+        new_data_pred_full[col] = data_pred_full[col]
     
-    
+    # save the new data_train_full and data_pred_full to csv files
+    new_data_train_full.to_csv("Datasets/" + "TF_" + dataset_name + "_train.csv", index=False)
+    new_data_pred_full.to_csv("Datasets/" + "TF_" + dataset_name + "_test.csv", index=False)
